461d720d8198ef87351459d75a0b6f817b9f6b1f
Alexey Kurylev <chepe4pi@gmail.com>
2024-07-20 21:44:16 -0300

    add Procfile and config

--- xqueue_watcher/grader.py
+++ xqueue_watcher/grader.py
@@ -113,7 +113,7 @@ class Grader:
             return {'score': 0, 'msg': "Invalid request: missing lesson_task_id", 'correct': None}
 
         try:
-            port = str(open('port', 'r').read()).strip()
+            port = os.environ.get('PORT', 5498)
             response = requests.post(
                 f'http://localhost:{port}/grade/',
                 json={'lesson_task_id': lesson_task_id, 'student_response': student_response}
